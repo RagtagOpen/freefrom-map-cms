a3ec8dfe945b1d3bad6dc86b42397cbc9b2e752a
Emily Giurleo <e.m.giurleo@gmail.com>
2021-02-10 21:44:03 -0500

    Move CMS to new repository

---
+++ src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'pages/home/Home.css';
+
+function Home() {
+    return (
+        <div>
+            <h1>Home Page</h1>
+        </div>
+    );
+}
+
+export default Home;
